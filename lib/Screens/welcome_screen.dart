47f27275a58d461b52f03adfbe14039d68548860
Mohit Soni <25mohitsoni@gmail.com>
2021-03-20 12:58:08 +0530

    created welcome screen

--- lib/Screens/welcome_screen.dart
+++ lib/Screens/welcome_screen.dart
@@ -1,20 +1,98 @@
 import 'package:flutter/material.dart';
+import 'package:netflix/Screens/login_screen.dart';
+import 'package:netflix/Screens/register_screen.dart';
 import 'package:netflix/authentication/authentication_service.dart';
 import 'package:provider/provider.dart';
 
 class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: TextButton(
-        onPressed: () {
-          context.read<AuthenticationService>().signIn(
-                email: 'test@gmail.com',
-                password: '123456',
-              );
-        },
-        child: Text('login'),
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Stack(
+          children: [
+            Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image(
+                    image: AssetImage('assets/logo.png'),
+                    width: 200.0,
+                  ),
+                  SizedBox(height: 25.0),
+                  Text(
+                    'WELCOME TO NETFLIX!',
+                    style: TextStyle(
+                      letterSpacing: 1.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 40.0),
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 32.0),
+                    height: 50.0,
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => RegisterScreen()),
+                        );
+                      },
+                      child: Text(
+                        'JOIN NOW',
+                        style: TextStyle(
+                          letterSpacing: 1.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.red,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              alignment: Alignment.bottomCenter,
+              padding: EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Already Have an Account?',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => LoginScreen()),
+                      );
+                    },
+                    child: Text(
+                      'LOGIN',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
+
+// context.read<AuthenticationService>().signIn(
+//       email: 'test@gmail.com',
+//       password: '123456',
+//     );
